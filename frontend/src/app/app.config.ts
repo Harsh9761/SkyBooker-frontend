f2fe9627cd19f647a8bbf61c174932b5ff569153
Harsh9761 <harshmathura8@gmail.com>
2026-05-13 12:38:58 +0530

    [Harshdeep Singh]Add. deployement updated

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,14 +1,15 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter,withHashLocation } from '@angular/router';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 
 import { routes } from './app.routes';
 import { authInterceptor } from './services/auth.interceptor';
 
+
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes),
+    provideRouter(routes, withHashLocation()),
 
     provideHttpClient(
       withInterceptors([authInterceptor])
